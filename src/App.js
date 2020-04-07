5635b261a2f871aa71ab1e0155d27d603d8036da
Leandro Tavares <leandro@phox.com.br>
2020-04-07 10:03:14 -0300

    Implementação de listagem, adição e remoção de itens

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const title = `Novo repositório ${Date.now()}`;
+    const url = 'https://github.com/leandro-tavares/gostack-desafio-conceitos-reactjs';
+    const techs = ['Node.js', 'React.js'];
+
+    const response = await api.post('repositories', { title, url, techs });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    console.log(repository);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete('repositories/' + id);
+    const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+
+    setRepositories(repositories.filter((repo, i) => repositoryIndex !== i));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li
+            key={repo.id}
+            style={{ width: '400px', padding: 10, margin: 10, border: '1px solid #999', borderRadius: 8 }}
+          >
+            <div style={{ width: '250px' }}>
+              <div>{repo.title}</div>
+              <div style={{ fontSize: 12, marginTop: 2 }}>{repo.id}</div>
+            </div>
+            <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
